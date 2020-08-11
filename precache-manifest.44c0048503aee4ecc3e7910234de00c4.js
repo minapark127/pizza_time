2905612bb1da24e13b05b22adcb3c9b6e91403cb
Mina <mina.park127@gmail.com>
2020-08-12 02:08:28 +1000

    Updates

---
+++ precache-manifest.44c0048503aee4ecc3e7910234de00c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48d8353497ee42d96c68fd9250a42e72",
+    "url": "/pizza_time/index.html"
+  },
+  {
+    "revision": "0d26d73dbee9bc2a03c3",
+    "url": "/pizza_time/static/js/2.56dccf35.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/pizza_time/static/js/2.56dccf35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7ed8ef02b856d5b5eb2",
+    "url": "/pizza_time/static/js/main.e29f477c.chunk.js"
+  },
+  {
+    "revision": "47163baa01df1894a817",
+    "url": "/pizza_time/static/js/runtime-main.ce4e0524.js"
+  },
+  {
+    "revision": "f6566347f275d48695e7c2845b8024e9",
+    "url": "/pizza_time/static/media/noPoster-sml.f6566347.png"
+  },
+  {
+    "revision": "381db2c7cb28116f42470be0082a3362",
+    "url": "/pizza_time/static/media/noPoster.381db2c7.png"
+  }
+]);
